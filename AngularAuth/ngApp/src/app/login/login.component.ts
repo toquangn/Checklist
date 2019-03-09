d014cc14c3a79b61d4e0bd5c4b6cff066b8359d1
Quang <quangto@Quangs-MacBook-Pro.local>
2019-03-09 00:55:19 -0800

    defined login component html

--- AngularAuth/ngApp/src/app/login/login.component.ts
+++ AngularAuth/ngApp/src/app/login/login.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginUserData = {}
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loginUser(){
+    console.log(this.loginUserData);
   }
 
 }
