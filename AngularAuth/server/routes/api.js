5d35ede695105fe399f6f7e6e301c438646a5f0b
Quang <quangto@Quangs-MacBook-Pro.local>
2019-03-09 11:26:44 -0800

    implemented jwt for tokenizing register/login

--- AngularAuth/server/routes/api.js
+++ AngularAuth/server/routes/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user');
@@ -25,7 +26,9 @@ router.post('/register', (req, res) =>{
       console.log('Save Error: ' + error);
     }
     else {
-      res.status(200).send(registeredUser);
+      let payload = {subject: registeredUser._id };
+      let token = jwt.sign(payload, 'secretKey');
+      res.status(200).send({ token });
     }
   });
 });
@@ -43,7 +46,9 @@ router.post('/login', (req, res) => {
           if(user.password !== userData.password){
             res.status(401).send('Invalid password');
           } else {
-            res.status(200).send(user);
+            let payload = { subject: user._id };
+            let token = jwt.sign(payload, 'secretKey');
+            res.status(200).send({ token });
           }
       }
     }
